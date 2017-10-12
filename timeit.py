dcd7a99d80ae4d0ca33413a8f84662a5a658bd1b
toransahu <toran.sahu@yahoo.com>
2017-10-12 12:47:35 +0530

    updated

--- timeit.py
+++ timeit.py
@@ -5,10 +5,13 @@ def exec_time(some_func):
     """ Track execution time of a function"""
     def wrapper(*args, **kwargs):
         ts = time.time()
-        print("Function started at ", ts)
-        some_func(*args, **kwargs)
+        print("\t======================================")
+        print("\tFunction started at", ts)
+        result = some_func(*args, **kwargs)
         te = time.time()
-        print("Function ended at ", te)
-        print("Execution time:",'%r  %2.2f ms' % 
+        print("\tFunction ended at", te)
+        print("\tExecution time of %r is %2.2f ms" % 
             (some_func.__name__, (te - ts) * 1000))
+        print("\t======================================")
+        return result
     return wrapper
