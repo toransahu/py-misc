1aee85e5871bdaa841c6042ca851d6405a3cd282
TORAN <toran.sahu@yahoo.com>
2018-03-23 12:43:35 +0530

    example - use of if __name__ == '__main__':

---
+++ example-__main__/module1.py
@@ -0,0 +1,5 @@
+def foo1():
+	print("I'm from module1")
+
+if __name__ == '__main__':
+	foo1()
---
+++ example-__main__/module2.py
@@ -0,0 +1,4 @@
+import module1
+
+print("Printing __name__ from module2.py: " + __name__)
+print("Printing module1.__name__ from module2.py: " + module1.__name__)
